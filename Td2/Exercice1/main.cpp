a78e8daee97eb9ace478abaa814c623df4ff37aa
Lililica <lucas.terrasson@gmail.com>
2024-03-02 15:55:38 +0100

    test v2

--- Td2/Exercice1/main.cpp
+++ Td2/Exercice1/main.cpp
@@ -6,9 +6,7 @@ bool is_sorted(std::vector<int> const& vec){
     return std::is_sorted(vec.begin(), vec.end()); 
 }
 
-void bubble_sort(std::vector<int> & vec);
 
-void selection_sort(std::vector<int> & vec);
 
 int main(){
     std::cout << "Exercice 1 - Td2" << std::endl;

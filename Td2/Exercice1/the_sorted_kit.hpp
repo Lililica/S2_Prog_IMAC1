2839d1d27552caffc9645603a7a9c86615cc2118
Lililica <geogold.11@gmail.com>
2024-03-10 17:14:30 +0100

    Td 2 fix v2

--- Td2/Exercice1/the_sorted_kit.hpp
+++ Td2/Exercice1/the_sorted_kit.hpp
@@ -11,6 +11,4 @@ void displayVec(std::vector<float> const& vec);
 // void quick_sort(std::vector<float> & vec, size_t const left, size_t const right);
 // void quick_sort(std::vector<float> & vec);
 
-// void merge_sort_merge(std::vector<float> & vec, size_t const left, size_t const middle, size_t const right);
 void merge_sort(std::vector<float> & vec, size_t const left, size_t const right);
-// void merge_sort(std::vector<float> & vec);

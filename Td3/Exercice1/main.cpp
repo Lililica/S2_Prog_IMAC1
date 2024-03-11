41f790bb94b7e22f06a21c9d07d79804ad904ddf
Lililica <lucas.terrasson@gmail.com>
2024-03-11 15:40:25 +0100

    Td3 v1

--- Td3/Exercice1/main.cpp
+++ Td3/Exercice1/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <cctype>
+
+std::vector<std::string> split_string(std::string const& s)
+{
+    std::istringstream in(s);
+    return std::vector<std::string>(std::istream_iterator<std::string>(in), std::istream_iterator<std::string>());
+}
+
+bool is_floating(std::string const& s){
+    
+    int i{0};
+
+    while(std::isdigit(s[i]) || s[i] == '.'){
+        if(i == s.size() - 1){
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+void display_vec(std::vector<std::string> vec){
+    for(std::string element : vec){
+        std::cout << element << std::endl;
+    }
+}
 
 int main(){
-    std::cout << "Exercice 1 - Td1" << std::endl;
+    std::cout << "Exercice 1 - Td3" << std::endl;
+    std::cout << std::endl;
+
+    std::string input;
+
+    std::cout << "Veuiller rentrer un calcul sous format NPI : ";
+    std::getline(std::cin, input);
+    std::cout << std::endl;
+
+    std::vector<std::string> mySplitString {split_string(input)};
+
+    for(std::string element : mySplitString){
+        std::cout << is_floating(element);
+    }
     std::cout << std::endl;
 
 }
